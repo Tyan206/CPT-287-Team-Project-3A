35171f1832baad9f0f0c29523e25552135fe6beb
Tyan206 <81524960+Tyan206@users.noreply.github.com>
2022-03-16 16:17:04 -0500

    Update Program.java

--- Team_Project_3A/src/binary_tree_infix_exp_parser/Program.java
+++ Team_Project_3A/src/binary_tree_infix_exp_parser/Program.java
@@ -1,286 +1,31 @@
 package binary_tree_infix_exp_parser;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.PriorityQueue;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.Set;
-import java.util.Stack;
+
 
 
 
 
 public class Program {
 
-
-	@SuppressWarnings("null")
-	public static BTNode postfixToTree(String postfix) {
-		BTNode expression = null, root = null;
-		Stack<BTNode> stk = new Stack<BTNode>();
-		Scanner scanner = new Scanner(postfix);
-
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		/*Main program where we will read in data from input file
+		  and print output to console */
+		FileInputStream inputfile = new FileInputStream("input.txt");
+		Scanner scanner = new Scanner(inputfile);
+		Infix_Parser parser;
+		String exp = "";
 		while(scanner.hasNext()) {
-			root = new BTNode(scanner.next());
-			if(Character.isDigit(root.data.charAt(0))) {
-				stk.push(root);
-				//System.out.println(root.data);
-			}else {
-				//System.out.println(root.data);
-				BTNode rightNode = stk.pop();
-				root.right =  rightNode;
-				BTNode leftNode = stk.pop();
-				root.left =  leftNode;
-				stk.push(root);
-			}
+			exp += scanner.nextLine().replace(" ", "").replace("\t", ""); // this will skip space and \t
+			parser = new Infix_Parser(exp);
+			System.out.println(parser.toString());
+			exp = "";
 		}
-		//System.out.println("root: " + root.data);
-	//	System.out.println("root left: " + root.left.data);
-	//	System.out.println("root right: " + root.right.data);
-	//	System.out.println("root: " + root.left.left.data);
 
-		while(!stk.empty()) {
-			root = stk.pop();
-		}
-		//System.out.println("root: " + root.left.left.data);
 
-		expression = root;
-		return expression;
-	}
-
-	/** Postorder traverses a binary tree.
-    @param root: the root node of a binary tree
-    @param <T>: Object type
-	 */
-	public static void postorderTraversal(BTNode root) {
-		if (root != null) {
-			postorderTraversal(root.left);
-			postorderTraversal(root.right);
-			System.out.print(root.data.toString() + ' ');
-		}
-	}
-
-	public static BTNode infixToTree(String input) {
-
-		// Stack to hold operands stN
-		Stack<BTNode> operands = new Stack<>();
-		// Stack to hold operators stC
-		Stack<String> operators = new Stack<>();
-
-		BTNode root, rightNode, leftNode;
-
-		// Map to priortising the operators
-
-		// Scanner through input
-		Scanner scanner = new Scanner(input);
-
-		while(scanner.hasNext()) {
-			String nextVal = scanner.next();
-			if(nextVal.equals("(")) {
-			//	System.out.println("First if: "+nextVal);
-				operators.add(nextVal);
-			}else if(Character.isDigit(nextVal.charAt(0))){
-			//	System.out.println("Second if: "+nextVal);
-				root = new BTNode(nextVal);
-				operands.add(root);
-			}else if(precedence(nextVal) > 0) {
-			//	System.out.println("Third if: "+nextVal);
-				// If an operator with lower or same associativity appears
-				while(!operators.empty() && !operators.peek().equals("(") && 
-						precedence(operators.peek()) >= precedence(nextVal)	 ) {
-					
-					/*
-					 
-					 ((!nextVal.equals("^") && precedence(operators.peek()) >= precedence(nextVal)) 
-								|| (nextVal.equals("^") && precedence(operators.peek()) > precedence(nextVal))) 
-					 
-					 
-				//	 */
-				//	System.out.println("Third if and loop: "+nextVal);
-					root = new BTNode(operators.pop());
-
-					rightNode = operands.pop();
-					
-					leftNode = operands.pop();
-					
-					root.left = leftNode;
-					root.right = rightNode;
-					
-					operands.add(root);
-				}
-				operators.add(nextVal);
-			}else if (nextVal.equals(")")) {
-				//System.out.println("Fourth if: "+nextVal);
-			//	System.out.println(operators.isEmpty());
-				while(!operators.isEmpty() && !operators.peek().equals("(")) {
-				//	System.out.println("Fourth if and loop: "+nextVal);
-					root = new BTNode(operators.pop());
-					
-					rightNode = operands.pop();
-					leftNode = operands.pop();
-					
-					root.left = leftNode;
-					root.right = rightNode;
-					
-					operands.add(root);
-				}
-				operators.pop();
-			}
-		}
-		root = new BTNode("NULL");
-		while(operands.size()>1) {
-			root = new BTNode(operators.pop());
-			
-			rightNode = operands.pop();
-			leftNode = operands.pop();
-			
-			root.left = leftNode;
-			root.right = rightNode;
-			
-			operands.add(root);
-			
-		}
-		return operands.peek();
-	}
-
-	/** Returns the precedence of an operator.
-    @param oper: operator to find its precedence
-    @return: precedence of the operator
-    @throws IllegalArgumentException: operator is not supported.
-	 */
-	public static int precedence(String oper) {
-		if(oper.equals(")")) { return 0;}
-		if (oper.equals("||")){ return 1; }
-		if (oper.equals("&&")){ return 2; }
-		if (oper.equals("==") || (oper.equals("!="))){ return 3; }
-		if (oper.equals(">") || (oper.equals(">=")) || (oper.equals("<")) || (oper.equals("<="))){ return 4; }
-		if (oper.equals("+") || (oper.equals("-"))){ return 5; }
-		if (oper.equals("*") || (oper.equals("/")) || (oper.equals("%"))){ return 6; }
-		if (oper.equals("^")){ return 7; }
-		throw new IllegalArgumentException("Operator not supported");
-	}
-
-	/** Converts an infix expression to postfix expression.
-    @param infixExp: infix expression to convert
-    @return: result postfix expression
-	 */
-	public static String infixToPostfix(String infixExp) {
-		Stack<String> stk = new Stack<>();
-		StringBuilder postfix = new StringBuilder();
-		Scanner scanner = new Scanner(infixExp);
-		while (scanner.hasNext()) {
-			String token = scanner.next();
-			if (Character.isDigit(token.charAt(0))) { postfix.append(token).append(' '); }
-			else if (token.equals("(")) { stk.push(token); }
-			else if (token.equals(")")) {
-				while (!stk.peek().equals("(")) { postfix.append(stk.pop()).append(' '); }
-				stk.pop();
-			} else {
-				while (!stk.isEmpty() && !stk.peek().equals("(") && precedence(token) <= precedence(stk.peek())) {
-					postfix.append(stk.pop()).append(' ');
-				}
-				stk.push(token);
-			}
-		}
-		while (!stk.isEmpty()) { postfix.append(stk.pop()).append(' '); }
-		scanner.close();
-		return postfix.toString();
-	}
-	
-/** Evaluates an expression using a expression tree. 
-	 * "^", "*", "/", "%", "+", "-", ">", ">=", "<", "<=", "==", "!=", "&&", "||" 14 totals
-	    @param root: tree to evaluate
-	    @return: evaluation result
-	*/
-	public static int evaluate(BTNode root){
-		if(root == null) return 0;
-		
-		if(root.left == null && root.right == null) return Integer.valueOf(root.data);
-		
-		//Evaluate left subtree
-		int evalLeft = evaluate(root.left);
-		
-		//Evaluate right subtree
-		int evalRight = evaluate(root.right);
-		
-		//Apply operators
-		if(root.data.equals("+")) return evalLeft + evalRight;
-		if(root.data.equals("-")) return evalLeft - evalRight;
-		if(root.data.equals("*")) return evalLeft * evalRight;
-		
-		if(root.data.equals("%")) return evalLeft % evalRight;
-		if(root.data.equals("^")) return evalLeft ^ evalRight;
-		if(root.data.equals(">")) {
-			if(evalLeft > evalRight) {
-				return 1;
-			}else return 0;
-		}
-		if(root.data.equals("<")) {
-			if(evalLeft < evalRight) {
-				return 1;
-			}else return 0;
-		}
-		if(root.data.equals(">=")) {
-			if(evalLeft >= evalRight) {
-				return 1;
-			}else return 0;
-		}
-		if(root.data.equals("<=")) {
-			if(evalLeft <= evalRight) {
-				return 1;
-			}else return 0;
-		}
-		if(root.data.equals("==")) {
-			if(evalLeft == evalRight) {
-				return 1;
-			}else return 0;
-		}
-		if(root.data.equals("!=")) {
-			if(evalLeft != evalRight) {
-				return 1;
-			}else return 0;
-		}
-		if(root.data.equals("&&")) {
-			if(evalLeft == 1 && evalRight == 1) {
-				return 1;
-			}else return 0;
-		}
-		if(root.data.equals("||")) {
-			if(evalLeft == 1 || evalRight == 1) {
-				return 1;
-			}else return 0;
-		}
-		
-		return evalLeft / evalRight;
 	}
 	
 	
-	public static void main(String[] args) {
-		String second = "( 6 + ( 8 / 2 ) ) * ( 3 - ( 1 - 3 ) )";
-		String infix = " ( 1 ^ 2 ^ ( 3 / 4 / 5 - 6 ) ^ ( 7 * 8 - 9 * 10 ) ) ";
-		String third = "( 4 >= 4 ) && 0";
-		String postfix = infixToPostfix(second);
-
-		BTNode test = postfixToTree(postfix);
-		BTNode test1 = infixToTree(second);
-		//System.out.println(test.left.left.data);
-		//System.out.println(infix);
-		System.out.println(postfix);
-		System.out.println();
-		postorderTraversal(test);
-		System.out.println();
-		//System.out.println(test1.data);
-		postorderTraversal(test1);
-		
-		System.out.println();
-		
-		System.out.println(eval(test1));
-		
-
-
-	}
-}
